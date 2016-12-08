c4305a18d925d14eb89c065f2e79b2c9bfb839de
sulianggang <sulianggang@askingdata.com>
2016-12-09 02:23:21 +0800

    第一种轮播效果

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,4 +1,26 @@
 angular.module('ctrlModule', [])
 	.controller('testCtrl', function ($scope, $http) {
-	    $scope.test = '测试'
+	    $scope.imgs = [
+			"https://contestimg.wish.com/api/webimage/5810997858bda0247cbf7559-orignal.jpg",
+			"https://contestimg.wish.com/api/webimage/5810997858bda0247cbf7559-1-orignal.jpg",
+			"https://contestimg.wish.com/api/webimage/5810997858bda0247cbf7559-2-orignal.jpg",
+			"https://contestimg.wish.com/api/webimage/5810997858bda0247cbf7559-3-orignal.jpg"
+		];
+		 $scope.imgIndex = 0;
+		$scope.selImg = function (type) {
+			if (type = '+') {
+				if($scope.imgIndex < $scope.imgs.length - 1){
+				  $scope.imgIndex++;
+				}else{
+				  $scope.imgIndex = 0;
+				}
+
+			} else{
+				if($scope.imgIndex > 0){
+				  $scope.imgIndex--;
+				}else{
+				  $scope.imgIndex = $scope.imgs.length - 1;
+				}
+			}
+		}
 	})
